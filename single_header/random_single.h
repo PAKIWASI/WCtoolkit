4a945edd4d99071c01fac90d0c86960e47f4f8c8
PAKIWASI <ullahwasi26@gmail.com>
2026-03-13 13:34:28 +0500

    update single header lib

--- single_header/random_single.h
+++ single_header/random_single.h
@@ -39,6 +39,8 @@
 #define COLOR_CYAN   "\033[1;36m"
 
 
+// TODO: warm paths ?
+
 #define WARN(fmt, ...)                                            \
     do {                                                          \
         printf(COLOR_YELLOW "[WARN]"                              \
