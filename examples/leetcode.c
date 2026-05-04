1e4581f5f8234d890aed6e8d51636e5740ac2b9d
PAKIWASI <ullahwasi26@gmail.com>
2026-05-04 22:35:50 +0500

    add leetcode exe

--- examples/leetcode.c
+++ examples/leetcode.c
@@ -1,15 +1,17 @@
 #include "common.h"
+#include <stdio.h>
 
 
 static int two_sum(const int* arr, u32 size, int target)
 {
 
+    printf("hello");
 }
 
 
 
 
-extern void leetcode_runner(void)
+int main(void)
 {
     int arr1[5] = { 1, 3, 6, 7, 2 };
     int res = two_sum(arr1, 5, 9);
