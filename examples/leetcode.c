004b6c9b6897eb9386f9585160a4b2c6de32b928
PAKIWASI <ullahwasi26@gmail.com>
2026-05-04 22:57:08 +0500

    example : two sums

--- examples/leetcode.c
+++ examples/leetcode.c
@@ -1,11 +1,36 @@
 #include "common.h"
-#include <stdio.h>
+#include "gen_vector.h"
+#include "hashmap.h"
+#include "wc_macros.h"
 
 
-static int two_sum(const int* arr, u32 size, int target)
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+static genVec* two_sum(const int* arr, u32 size, int target)
 {
+    hashmap* map = hashmap_create( sizeof(int),
+                sizeof(u32), NULL, NULL, NULL, NULL );
+    genVec* v = VEC(u32, 2);
 
-    printf("hello");
+    for (u32 i = 0; i < size; i++) {
+        int remaining = target - arr[i];
+        u8* idx = hashmap_get_ptr(map, cast(remaining));
+        if (idx) {
+            genVec_push(v, cast(i));
+            break;
+        }
+        MAP_PUT(map, cast(arr[i]), cast(i));
+    }
+
+    hashmap_destroy(map);
+    return v;
 }
 
 
@@ -13,7 +38,11 @@ static int two_sum(const int* arr, u32 size, int target)
 
 int main(void)
 {
-    int arr1[5] = { 1, 3, 6, 7, 2 };
-    int res = two_sum(arr1, 5, 9);
+    int arr1[4] = {2, 7, 11, 15};
+    genVec* res = two_sum(arr1, 4, 9);
+    genVec_print(res, wc_print_u32);
 
+
+    genVec_destroy(res);
+    return 0;
 }
