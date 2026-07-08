1a855f50beaa8bf208562cc5daf6bd8cfe7867ed
PAKIWASI <ullahwasi26@gmail.com>
2026-07-08 19:52:51 +0500

    small fix

--- src/main.c
+++ src/main.c
@@ -1,29 +1,29 @@
-#include "String.h"
+#include "gen_vector.h"
 #include "hashmap.h"
+
 #include "wc_helpers.h"
 #include "wc_macros.h"
 
 
-
-
-
 int main(void)
 {
-    hashmap* map = hashmap_create(
-        sizeof(String),
-        sizeof(String),
-        wyhash_str,
-        str_cmp,
-        &wc_str_ops,
-        &wc_str_ops
+    hashmap* map  = hashmap_create(
+            sizeof(String),
+            sizeof(genVec),
+            wyhash_str,
+            str_cmp,
+            &wc_str_ops,
+            &wc_vec_ops
     );
 
-    MAP_PUT_STR_STR(map, "hello", "world");
-    MAP_PUT_STR_STR(map, "hello", "world_2");
-    MAP_PUT_STR_STR(map, "wtf", "skjfdkfj");
-    MAP_PUT_STR_STR(map, "a", "A");
+    String* s = string_from_cstr("hello");
+    genVec* v = genVec_init_arr(5, sizeof(int), NULL, 
+            (u8*)(int[5]){1, 2, 3, 4, 5});
 
-    hashmap_print(map, str_print, str_print);
+    MAP_PUT_MOVE(map, s, v);
 
+    hashmap_print(map, str_print, vec_print_int);
+
+    hashmap_destroy(map);
     return 0;
 }
