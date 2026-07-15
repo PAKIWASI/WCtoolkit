d7e2104ec0dacbbfeb8e57e0e8e40c7199863cf8
PAKIWASI <ullahwasi26@gmail.com>
2026-07-16 01:27:42 +0500

    f

--- src/main.c
+++ src/main.c
@@ -1,17 +1,10 @@
-#include "arena.h"
+#include "chain_arena.h"
 
 
 int main(void)
 {
-    Arena* arena = arena_create(0);
+    ChainArena* arena = chain_arena_create();
 
-    ARENA_SCRATCH(arena) {
-        int* n = ARENA_ALLOC_N(arena, int, 10);
 
-        n[0] = 67;
-    }
-
-    printf("%lu\n", arena_remaining(arena));
-    arena_release(arena);
-    return 0;
+    chain_arena_release(arena);
 }
